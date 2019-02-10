7e41970e73ebb0588402d2f86000eefe04d9cddd
Mateusz <mateusz.bieda95@gmail.com>
2019-02-10 21:25:04 +0100

    Add list of products, use context Api

--- src/index.js
+++ src/index.js
@@ -2,8 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import {BrowserRouter as Router} from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { ProductProvider } from './context'
 ReactDOM.render(
-    <Router><App /></Router>,
- document.getElementById('root'));
+    <ProductProvider>
+        <Router>
+            <App />
+        </Router>
+    </ProductProvider>,
+    document.getElementById('root'));
 
